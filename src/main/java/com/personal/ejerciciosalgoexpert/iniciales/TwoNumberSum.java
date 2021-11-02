feb813d263d05cf32efb369d44e791f2a4e82dcc
Daniel López <daniel.lopezing20@gmail.com>
2021-11-01 19:54:40 -0600

    Se agrega respuesta con mejor desempeño.

--- src/main/java/com/personal/ejerciciosalgoexpert/iniciales/TwoNumberSum.java
+++ src/main/java/com/personal/ejerciciosalgoexpert/iniciales/TwoNumberSum.java
@@ -1,5 +1,6 @@
 package com.personal.ejerciciosalgoexpert.iniciales;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,7 +19,8 @@ public class TwoNumberSum {
      * [-1, 11]
      **/
 
-    public static int[] twoNumberSum(int[] array, int targetSum) {
+    public static int[] twoNumberSum2(int[] array, int targetSum) {
+        /* O(n) time | O(n) space */
         Set<Integer> nums = new HashSet<>();
         for(int num : array){
             int potentialMatch = targetSum - num;
@@ -27,6 +29,20 @@ public class TwoNumberSum {
             } else{
                 nums.add(num);
             }
+        }
+        return new int[0];
+    }
+
+    public static int[] twoNumberSum(int[] array, int targetSum){
+        /* O(nlog(n)) time | O(1) space */
+        Arrays.sort(array);
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right){
+            int currentSum = array[left] + array[right];
+            if(currentSum == targetSum) return new int[]{array[left], array[right]};
+            else if(currentSum < targetSum) left++;
+            else right--;
         }
         return new int[0];
     }
